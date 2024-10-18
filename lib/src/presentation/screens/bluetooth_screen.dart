0b49712e8ae6a022e9c0892d00f5a578d82d4a48
Fady <fedo.zaher@gmail.com>
2024-10-18 11:40:25 +0300

    fix

--- lib/src/presentation/screens/bluetooth_screen.dart
+++ lib/src/presentation/screens/bluetooth_screen.dart
@@ -1,30 +1,31 @@
-// ignore_for_file: avoid_print, must_be_immutable
+// ignore_for_file: avoid_print
+
+import 'dart:typed_data';
 import 'package:fatora/src/config/theme/color_schemes.dart';
 import 'package:fatora/src/core/resources/image_paths.dart';
 import 'package:fatora/src/core/utils/permission_service_handler.dart';
 import 'package:fatora/src/core/utils/show_action_dialog_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:esc_pos_utils/esc_pos_utils.dart';
-import 'package:esc_pos_printer/esc_pos_printer.dart';
-import 'dart:typed_data';
-import 'package:flutter/services.dart';
 import 'package:image/image.dart' as img;
 import 'package:permission_handler/permission_handler.dart';
 
 class PrintScreen extends StatefulWidget {
   final Uint8List imageBytes;
 
-  const PrintScreen({super.key, required this.imageBytes});
+  const PrintScreen({
+    super.key,
+    required this.imageBytes,
+  });
 
   @override
-  _PrintScreenState createState() => _PrintScreenState();
+  State<PrintScreen> createState() => _PrintScreenState();
 }
 
 class _PrintScreenState extends State<PrintScreen> {
   FlutterBlue flutterBlue = FlutterBlue.instance;
-  List<BluetoothDevice> devicesList = [];
   BluetoothDevice? connectedDevice;
+  List<BluetoothDevice> discoveredDevices = [];
 
   @override
   void didChangeDependencies() async {
@@ -35,93 +36,79 @@ class _PrintScreenState extends State<PrintScreen> {
   // Request Bluetooth permissions
   Future<void> requestPermissions() async {
     // Request the necessary Bluetooth permissions
-    // var status = await Permission.bluetoothScan.request();
-    if (await PermissionServiceHandler()
-            .handleServicePermission(setting: Permission.bluetoothConnect) &&
-        await PermissionServiceHandler()
-            .handleServicePermission(setting: Permission.bluetoothScan)) {
-      // If granted, start scanning for devices
-      scanForDevices();
-    } else {
-      _dialogMessage(
-          icon: ImagePaths.warning,
-          message: "Bluetooth Scan permission denied",
-          primaryAction: () async {
-            Navigator.pop(context);
-            openAppSettings().then((value) async {
-              if (await PermissionServiceHandler().handleServicePermission(
-                      setting: Permission.bluetoothConnect) &&
-                  await PermissionServiceHandler().handleServicePermission(
-                      setting: Permission.bluetoothScan)) {}
+
+    if (mounted) {
+      if (await PermissionServiceHandler()
+              .handleServicePermission(setting: Permission.bluetoothConnect) &&
+          await PermissionServiceHandler()
+              .handleServicePermission(setting: Permission.bluetoothScan)) {
+        // If granted, start scanning for devices
+        await scanForDevices();
+      } else {
+        _dialogMessage(
+            icon: ImagePaths.warning,
+            message: "Bluetooth Scan permission denied",
+            primaryAction: () async {
+              Navigator.pop(context);
+              openAppSettings().then((value) async {
+                if (await PermissionServiceHandler().handleServicePermission(
+                        setting: Permission.bluetoothConnect) &&
+                    await PermissionServiceHandler().handleServicePermission(
+                        setting: Permission.bluetoothScan)) {}
+              });
             });
-          });
-      // Handle the case when the user denies the permission
-      print("Bluetooth Scan permission denied");
+        // Handle the case when the user denies the permission
+        print("Bluetooth Scan permission denied");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: ColorSchemes.white,
+            content: Text(
+              'تم رفض الاذن',
+              style: TextStyle(color: ColorSchemes.primary),
+            ),
+          ),
+        );
+      }
     }
   }
 
-  void scanForDevices() {
+  // Scan for Bluetooth devices
+  Future<void> scanForDevices() async {
+    // Start scanning
     flutterBlue.startScan(timeout: const Duration(seconds: 4));
-    flutterBlue.scanResults.listen((results) {
-      for (var result in results) {
-        if (!devicesList.contains(result.device)) {
-          setState(() {
-            devicesList.add(result.device);
-          });
-        }
+
+    // Listen to scan results
+    flutterBlue.scanResults.listen((scanResults) {
+      for (ScanResult result in scanResults) {
+        discoveredDevices.add(result.device);
+        setState(() {});
+        print('Found device: ${result.device.name}');
+        print('Found device: ${result.device.id}');
+        print("length: ${discoveredDevices.length}");
       }
     });
+
+    // Stop scanning after finding a device
+    flutterBlue.stopScan();
   }
 
-  void connectToDevice(BluetoothDevice device) async {
+  // Connect to a Bluetooth device
+  Future<void> connectToDevice(BluetoothDevice device) async {
+    connectedDevice = device;
     await device.connect();
-    setState(() {
-      connectedDevice = device;
-    });
+    print('Connected to ${device.name}');
     ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text("  تم الاتصال بالجهاز من فضلك اضغط طباعة الفاتورة"),
-        backgroundColor: Colors.green,
+      SnackBar(
+        backgroundColor: ColorSchemes.white,
+        content: Text(
+          'تم الاتصال بنجاح',
+          style: TextStyle(color: ColorSchemes.primary),
+        ),
       ),
     );
+    setState(() {});
   }
 
-  // Future<void> printImage() async {
-  //   if (connectedDevice != null) {
-  //     final profile = await CapabilityProfile.load();
-  //     final printer = NetworkPrinter(PaperSize.mm80, profile);
-  //
-  //     // Connect to printer
-  //     final connectResult =
-  //     await printer.connect(connectedDevice!.id.toString(), port: 9100);
-  //
-  //     if (connectResult == PosPrintResult.success) {
-  //       // Print the image
-  //       printer.image(
-  //         img.decodeImage(widget.imageBytes)!,
-  //         align: PosAlign.center,
-  //       );
-  //       printer.feed(2); // Feed the paper after printing
-  //       printer.disconnect();
-  //     } else {
-  //       print("لا يمكن الاتصال بالطابعة");
-  //       //SnackBar
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         const SnackBar(
-  //           content: Text("لم يتم الاتصال بالجهاز"),
-  //           backgroundColor: Colors.red,
-  //         ),
-  //       );
-  //     }
-  //   } else {
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(
-  //         content: Text("لم يتم الاتصال بالجهاز"),
-  //         backgroundColor: Colors.red,
-  //       ),
-  //     );
-  //   }
-  // }
   // Print a Uint8List image
   void printImage(Uint8List imageData) async {
     if (connectedDevice == null) {
@@ -203,6 +190,15 @@ class _PrintScreenState extends State<PrintScreen> {
     return null;
   }
 
+  // Disconnect from the Bluetooth device
+  Future<void> disconnectDevice() async {
+    if (connectedDevice != null) {
+      await connectedDevice!.disconnect();
+      connectedDevice = null;
+      print('Device disconnected');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -220,14 +216,14 @@ class _PrintScreenState extends State<PrintScreen> {
                 color: Theme.of(context).colorScheme.primary,
               ),
               const SizedBox(height: 10),
-              if (devicesList.isEmpty) const LinearProgressIndicator(),
+              if (discoveredDevices.isEmpty) const LinearProgressIndicator(),
               Expanded(
                 child: ListView.builder(
-                  itemCount: devicesList.length,
+                  itemCount: discoveredDevices.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: Text(devicesList[index].name.toString()),
-                      onTap: () => connectToDevice(devicesList[index]),
+                      title: Text(discoveredDevices[index].name.toString()),
+                      onTap: () => connectToDevice(discoveredDevices[index]),
                     );
                   },
                 ),
